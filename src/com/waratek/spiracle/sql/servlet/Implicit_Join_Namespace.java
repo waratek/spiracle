72c3a55c9759cbda8d1f54c8b6fff9b32772034f
skenny <skenny@ws18.waratek.lan>
2014-12-19 15:59:00 +0000

    Added table namespace identified select.

---
+++ src/com/waratek/spiracle/sql/servlet/Implicit_Join_Namespace.java
@@ -0,0 +1,65 @@
+package com.waratek.spiracle.sql.servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.waratek.spiracle.sql.servlet.util.ParameterNullFix;
+import com.waratek.spiracle.sql.util.SelectUtil;
+
+/**
+ * Servlet implementation class Implicit_Join_Namespace
+ */
+@WebServlet("/Implicit_Join_Namespace")
+public class Implicit_Join_Namespace extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Implicit_Join_Namespace() {
+        super();
+        // TODO Auto-generated constructor stub
+        //select users.id from users, address where
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        executeRequest(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        executeRequest(request, response);
+	}
+
+    private void executeRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {          
+        ServletContext application = this.getServletConfig().getServletContext();
+        List<String> queryStringList = new ArrayList<String>();     
+        queryStringList.add("id");
+        
+        Map<String, String> nullSanitizedMap = ParameterNullFix.sanitizeNull(queryStringList, request);
+        
+        String id = nullSanitizedMap.get("id");
+        
+        String sql = "SELECT users.id FROM users, address WHERE users.id = address.id AND users.id = " + id; 
+
+        Boolean showErrors = true;
+        Boolean allResults = true;
+        Boolean showOutput = true;
+
+        SelectUtil.executeQuery(sql, application, request, response, showErrors, allResults, showOutput);
+    }
+}
