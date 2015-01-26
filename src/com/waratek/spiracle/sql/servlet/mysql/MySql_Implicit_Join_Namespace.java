647813e12e6352e8570202365a1900d6b7dfcbe6
skenny <skenny@ws18.waratek.lan>
2015-01-26 11:41:06 +0000

    Added MySQL targeted injection servlets.

---
+++ src/com/waratek/spiracle/sql/servlet/mysql/MySql_Implicit_Join_Namespace.java
@@ -0,0 +1,64 @@
+package com.waratek.spiracle.sql.servlet.mysql;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.waratek.spiracle.sql.servlet.util.ParameterNullFix;
+import com.waratek.spiracle.sql.util.SelectUtil;
+
+/**
+ * Servlet implementation class Implicit_Join_Namespace
+ */
+@WebServlet("/MySql_Implicit_Join_Namespace")
+public class MySql_Implicit_Join_Namespace extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MySql_Implicit_Join_Namespace() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        executeRequest(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        executeRequest(request, response);
+	}
+
+    private void executeRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {          
+        ServletContext application = this.getServletConfig().getServletContext();
+        List<String> queryStringList = new ArrayList<String>();     
+        queryStringList.add("id");
+        
+        Map<String, String> nullSanitizedMap = ParameterNullFix.sanitizeNull(queryStringList, request);
+        
+        String id = nullSanitizedMap.get("id");
+        
+        String sql = "SELECT * FROM test.users, test.address WHERE test.users.id = " + id + " AND test.users.id = test.address.id"; 
+
+        Boolean showErrors = true;
+        Boolean allResults = true;
+        Boolean showOutput = true;
+
+        SelectUtil.executeQuery(sql, application, request, response, showErrors, allResults, showOutput);
+    }
+}
