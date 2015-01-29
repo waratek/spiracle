f308725b5b9165b86e4725252ec752211f01347c
skenny <skenny@ws18.waratek.lan>
2015-01-29 14:55:23 +0000

    Organise imports, cleanup whitespace.

--- src/com/waratek/spiracle/sql/util/UpdateUtil.java
+++ src/com/waratek/spiracle/sql/util/UpdateUtil.java
@@ -27,74 +27,72 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 
-import com.waratek.spiracle.init.SpiracleInit;
-
 public class UpdateUtil {
 	private static final Logger logger = Logger.getLogger(UpdateUtil.class);
 
-    public static void executeUpdate(String sql, ServletContext application, HttpServletRequest request, HttpServletResponse response) throws IOException{
-    	response.setHeader("Content-Type", "text/html;charset=UTF-8");
-        ServletOutputStream out = response.getOutputStream();
-        String connectionType = null;
-        Connection con = null;
+	public static void executeUpdate(String sql, ServletContext application, HttpServletRequest request, HttpServletResponse response) throws IOException{
+		response.setHeader("Content-Type", "text/html;charset=UTF-8");
+		ServletOutputStream out = response.getOutputStream();
+		String connectionType = null;
+		Connection con = null;
 
-        TagUtil.printPageHead(out);
-        TagUtil.printPageNavbar(out);
-        TagUtil.printContentDiv(out);
-        
-        try {
-            //Checking if connectionType is not, defaulting it to c3p0 if not set.
-            if(request.getParameter("connectionType") == null) {
-                connectionType = "c3p0";
-            } else {
-                connectionType = request.getParameter("connectionType");
-            }
-            con = ConnectionUtil.getConnection(application, connectionType);            
-            out.println("<div class=\"panel-body\">");
-            out.println("<h1>SQL Query:</h1>");
-            out.println("<pre>");
-            out.println(sql);
-            out.println("</pre>");
+		TagUtil.printPageHead(out);
+		TagUtil.printPageNavbar(out);
+		TagUtil.printContentDiv(out);
 
-            logger.info(sql);
+		try {
+			//Checking if connectionType is not, defaulting it to c3p0 if not set.
+			if(request.getParameter("connectionType") == null) {
+				connectionType = "c3p0";
+			} else {
+				connectionType = request.getParameter("connectionType");
+			}
+			con = ConnectionUtil.getConnection(application, connectionType);
+			out.println("<div class=\"panel-body\">");
+			out.println("<h1>SQL Query:</h1>");
+			out.println("<pre>");
+			out.println(sql);
+			out.println("</pre>");
 
-            PreparedStatement stmt = con.prepareStatement(sql);
-            int result = stmt.executeUpdate();
-            out.println("<h1>Altered Rows:</h1>");
-            out.print("<pre>" + result + "</pre>");
-            TagUtil.printPageFooter(out);
-            out.close();
-            stmt.close();
-            con.close();
-            
-        } catch(SQLException e) {
-            if(e.getMessage().equals("Attempted to execute a query with one or more bad parameters.")) {
-                response.setStatus(550);
-            } else {
-                response.setStatus(500);
-            }
-            out.println("<div class=\"alert alert-danger\" role=\"alert\">");
-            out.println("<strong>SQLException:</strong> " + e.getMessage() + "<BR>");
+			logger.info(sql);
+
+			PreparedStatement stmt = con.prepareStatement(sql);
+			int result = stmt.executeUpdate();
+			out.println("<h1>Altered Rows:</h1>");
+			out.print("<pre>" + result + "</pre>");
+			TagUtil.printPageFooter(out);
+			out.close();
+			stmt.close();
+			con.close();
+
+		} catch(SQLException e) {
+			if(e.getMessage().equals("Attempted to execute a query with one or more bad parameters.")) {
+				response.setStatus(550);
+			} else {
+				response.setStatus(500);
+			}
+			out.println("<div class=\"alert alert-danger\" role=\"alert\">");
+			out.println("<strong>SQLException:</strong> " + e.getMessage() + "<BR>");
 			if(logger.isDebugEnabled()) {
 				logger.debug(e.getMessage(), e);
 			} else {
 				logger.error(e);
 			}
-            while((e = e.getNextException()) != null) {
-                out.println(e.getMessage() + "<BR>");
-            }
-            try {
-                con.rollback();
-                con.close();
-            } catch (SQLException e1) {
-    			if(logger.isDebugEnabled()) {
-    				logger.debug(e.getMessage(), e);
-    			} else {
-    				logger.error(e);
-    			}
-            }
-            out.println("</div>");
-            TagUtil.printPageFooter(out);
-        }
-    }
+			while((e = e.getNextException()) != null) {
+				out.println(e.getMessage() + "<BR>");
+			}
+			try {
+				con.rollback();
+				con.close();
+			} catch (SQLException e1) {
+				if(logger.isDebugEnabled()) {
+					logger.debug(e.getMessage(), e);
+				} else {
+					logger.error(e);
+				}
+			}
+			out.println("</div>");
+			TagUtil.printPageFooter(out);
+		}
+	}
 }
