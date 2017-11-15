9beb5ff65ec1cd62ff14b27b47a2157220621d84
Christopher Tognini <ctognini@ws24.waratek.lan>
2017-11-15 16:42:30 +0000

    Add SQL servlets for query containing '?' character

--- src/main/java/com/waratek/spiracle/sql/servlet/mssql/MsSql_Get_string_param_question_mark.java
+++ src/main/java/com/waratek/spiracle/sql/servlet/mssql/MsSql_Get_string_param_question_mark.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2014 Waratek Ltd.
+ *  Copyright 2017 Waratek Ltd.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package com.waratek.spiracle.sql.servlet.mysql;
+package com.waratek.spiracle.sql.servlet.mssql;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -33,14 +33,14 @@ import com.waratek.spiracle.sql.util.SelectUtil;
 /**
  * Servlet implementation class Get_string
  */
-@WebServlet("/MySql_Get_string")
-public class MySql_Get_string extends HttpServlet {
+@WebServlet("/MsSql_Get_string_param_question_mark")
+public class MsSql_Get_string_param_question_mark extends HttpServlet {
     private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public MySql_Get_string() {
+    public MsSql_Get_string_param_question_mark() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -48,33 +48,35 @@ public class MySql_Get_string extends HttpServlet {
     /**
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         executeRequest(request, response);
     }
 
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    @Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         executeRequest(request, response);
     }
 
-    private void executeRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {          
+    private void executeRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
         ServletContext application = this.getServletConfig().getServletContext();
-        List<String> queryStringList = new ArrayList<String>();     
+        List<String> queryStringList = new ArrayList<String>();
         queryStringList.add("name");
-        
+
         Map<String, String> nullSanitizedMap = ParameterNullFix.sanitizeNull(queryStringList, request);
 
-        String name = nullSanitizedMap.get("name");
-        
-        String sql = "SELECT * FROM users WHERE name = '" + name + "'";
+        String param = nullSanitizedMap.get("name");
+
+        String sql = "SELECT top 5 id, name, surname FROM users where name <> ? and name = '" + param + "'";
 
         Boolean showErrors = true;
         Boolean allResults = true;
         Boolean showOutput = true;
 
-        SelectUtil.executeQuery(sql, application, request, response, showErrors, allResults, showOutput);
+        SelectUtil.executeQuerySetString(sql, application, request, response, showErrors, allResults, showOutput);
     }
 
 }
--- src/main/java/com/waratek/spiracle/sql/util/SelectUtil.java
+++ src/main/java/com/waratek/spiracle/sql/util/SelectUtil.java
@@ -66,72 +66,68 @@ public class SelectUtil {
 
 			stmt = con.prepareStatement(sql);
 			logger.info("Created PreparedStatement: " + sql);
-			stmt.setFetchSize(fetchSize);
-			rs = stmt.executeQuery();
-			logger.info("Executed: " + sql);
-
-			writeToResponse(allResults, showOutput, out, rs);
-		} catch(SQLException e) {
-			if(e.getMessage().equals("Attempted to execute a query with one or more bad parameters.")) {
-                int error = Integer.parseInt((String) application.getAttribute("defaultError"));
-				response.setStatus(error);
-			} else {
-				response.setStatus(500);
-			}
-			out.println("<div class=\"alert alert-danger\" role=\"alert\">");
-			out.println("<strong>SQLException:</strong> " + e.getMessage() + "<BR>");
-			if(logger.isDebugEnabled()) {
-				logger.debug(e.getMessage(), e);
-			} else {
-				logger.error(e);
-			}
-			while((e = e.getNextException()) != null) {
-				out.println(e.getMessage() + "<BR>");
-			}
+			executePreparedStatement(stmt, fetchSize, rs, sql, out, allResults, showOutput);
+		} catch(SQLException sqlexception) {
+			verifySQLException(sqlexception, application, response, out);
 		} finally {
-			try {
-				if(rs != null) {
-					logger.info("Closing ResultSet " + rs);
-					rs.close();
-					logger.info("Closed ResultSet " + rs);
-				}
-			} catch (SQLException rsCloseException) {
-				if(logger.isDebugEnabled()) {
-					logger.debug(rsCloseException.getMessage(), rsCloseException);
-				} else {
-					logger.error(rsCloseException);
-				}
-			}
-			try {
-				if(stmt != null) {
-					logger.info("Closing PreparedStatement " + stmt);
-					stmt.close();
-					logger.info("Closed PreparedStatement " + stmt);
-				}
-			} catch (SQLException stmtCloseException) {
-				if(logger.isDebugEnabled()) {
-					logger.debug(stmtCloseException.getMessage(), stmtCloseException);
-				} else {
-					logger.error(stmtCloseException);
-				}
-			}
-			try {
-				if(con != null) {
-					logger.info("Closing Connection " + con);
-					con.close();
-					logger.info("Closed Connection " + con);
-				}
-			} catch (SQLException conCloseException) {
-				if(logger.isDebugEnabled()) {
-					logger.debug(conCloseException.getMessage(), conCloseException);
-				} else {
-					logger.error(conCloseException);
-				}
-			}
+			cleanup(rs, stmt, con);
 			TagUtil.printPageFooter(out);
 			out.close();
 		}
 	}
+
+        	public static void executeQuerySetString(String sql, ServletContext application, HttpServletRequest request, HttpServletResponse response, Boolean showErrors, Boolean allResults, Boolean showOutput) throws IOException {
+		response.setHeader("Content-Type", "text/html;charset=UTF-8");
+		ServletOutputStream out = response.getOutputStream();
+		String connectionType = null;
+		Connection con = null;
+		int fetchSize = (Integer) application.getAttribute(Constants.JDBC_FETCH_SIZE);
+		String defaultConnection = (String) application.getAttribute(Constants.DEFAULT_CONNECTION);
+
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+
+		TagUtil.printPageHead(out);
+		TagUtil.printPageNavbar(out);
+		TagUtil.printContentDiv(out);
+
+		try {
+			//Checking if connectionType is set, defaulting it to c3p0 if not set.
+			if(request.getParameter("connectionType") == null) {
+				connectionType = defaultConnection;
+			} else {
+				connectionType = request.getParameter("connectionType");
+			}
+			con = ConnectionUtil.getConnection(application, connectionType);
+			out.println("<div class=\"panel-body\">");
+			out.println("<h1>SQL Query:</h1>");
+			out.println("<pre>");
+			out.println(sql);
+			out.println("</pre>");
+
+			logger.info(sql);
+
+			stmt = con.prepareStatement(sql);
+			logger.info("Created PreparedStatement: " + sql);
+                        stmt.setString(1, "something");
+                        logger.info("Substituted parameter in PreparedStatement: " + sql);
+			executePreparedStatement(stmt, fetchSize, rs, sql, out, allResults, showOutput);
+		} catch(SQLException sqlexception) {
+			verifySQLException(sqlexception, application, response, out);
+		} finally {
+			cleanup(rs, stmt, con);
+			TagUtil.printPageFooter(out);
+			out.close();
+		}
+	}
+
+        private static void executePreparedStatement(PreparedStatement stmt, int fetchSize, ResultSet rs, String sql, ServletOutputStream out, Boolean allResults, Boolean showOutput) throws IOException, SQLException {
+            stmt.setFetchSize(fetchSize);
+            rs = stmt.executeQuery();
+            logger.info("Executed: " + sql);
+
+            writeToResponse(allResults, showOutput, out, rs);
+        }
 
 	private static void writeToResponse(Boolean allResults, Boolean showOutput, ServletOutputStream out, ResultSet rs) throws SQLException, IOException {
 		ResultSetMetaData metaData = rs.getMetaData();
@@ -172,5 +168,71 @@ public class SelectUtil {
 			}
 		}
 		out.println("</TR>");
+	}
+
+        private static void verifySQLException(SQLException sqlexception, ServletContext application, HttpServletResponse response, ServletOutputStream out) throws IOException{
+                if(sqlexception.getMessage().equals("Attempted to execute a query with one or more bad parameters.")) {
+                        int error = Integer.parseInt((String) application.getAttribute("defaultError"));
+                        response.setStatus(error);
+                } else {
+                        response.setStatus(500);
+                }
+
+                out.println("<div class=\"alert alert-danger\" role=\"alert\">");
+                out.println("<strong>SQLException:</strong> " + sqlexception.getMessage() + "<BR>");
+
+                if(logger.isDebugEnabled()) {
+                        logger.debug(sqlexception.getMessage(), sqlexception);
+                } else {
+                        logger.error(sqlexception);
+                }
+
+                while((sqlexception = sqlexception.getNextException()) != null) {
+                        out.println(sqlexception.getMessage() + "<BR>");
+            }
+        }
+
+        private static void cleanup(ResultSet rs, PreparedStatement stmt, Connection con) throws IOException{
+                try {
+        	        if(rs != null) {
+                                logger.info("Closing ResultSet " + rs);
+                                rs.close();
+                                logger.info("Closed ResultSet " + rs);
+                        }
+                } catch (SQLException rsCloseException) {
+                        if(logger.isDebugEnabled()) {
+                                logger.debug(rsCloseException.getMessage(), rsCloseException);
+                        } else {
+                                logger.error(rsCloseException);
+                        }
+                }
+
+                try {
+        	        if(stmt != null) {
+                	            logger.info("Closing PreparedStatement " + stmt);
+                                    stmt.close();
+                                    logger.info("Closed PreparedStatement " + stmt);
+                        }
+                } catch (SQLException stmtCloseException) {
+                        if(logger.isDebugEnabled()) {
+                                logger.debug(stmtCloseException.getMessage(), stmtCloseException);
+                        } else {
+                                logger.error(stmtCloseException);
+                        }
+                }
+
+                try {
+                        if(con != null) {
+                                logger.info("Closing Connection " + con);
+                                con.close();
+                                logger.info("Closed Connection " + con);
+                        }
+                } catch (SQLException conCloseException) {
+                        if(logger.isDebugEnabled()) {
+                                logger.debug(conCloseException.getMessage(), conCloseException);
+                        } else {
+                                logger.error(conCloseException);
+                        }
+                }
 	}
 }
