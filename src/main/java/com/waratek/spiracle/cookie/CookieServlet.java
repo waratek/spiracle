efb7bcb8c164a46d9f4125d86904db0fdc333651
Conor O'Neill <coneill@waratek.com>
2018-08-07 15:55:49 +0100

    adds simple jsp / servlet to request server send back a cookie in the response

---
+++ src/main/java/com/waratek/spiracle/cookie/CookieServlet.java
@@ -0,0 +1,43 @@
+package com.waratek.spiracle.cookie;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/CookieServlet")
+public class CookieServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        String name = req.getParameter("cookieName");
+        String value = req.getParameter("cookieValue");
+        String secure = req.getParameter("secure");
+        String path = req.getParameter("cookiePath");
+
+        Cookie cookie = new Cookie(name, value);
+        if (secure.equals("yes")) {
+            cookie.setSecure(true);
+        }
+
+        if (path != null) {
+            cookie.setPath(path);
+        }
+
+        res.addCookie(cookie);
+
+        RequestDispatcher requestDispatcher = req
+                .getRequestDispatcher("/cookie.jsp");
+        requestDispatcher.forward(req, res);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
