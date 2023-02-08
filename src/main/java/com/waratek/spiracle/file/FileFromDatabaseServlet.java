6fd01bb211581dee897093b8d84fd9bce27b572e
Donnchadh Mac Suibhne <dmacsuibhne@waratek.com>
2023-02-08 17:37:21 +0000

    Remove unused imports

--- src/main/java/com/waratek/spiracle/file/FileFromDatabaseServlet.java
+++ src/main/java/com/waratek/spiracle/file/FileFromDatabaseServlet.java
@@ -23,7 +23,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
--- src/main/java/com/waratek/spiracle/file/FileFromJavaDeserializationServlet.java
+++ src/main/java/com/waratek/spiracle/file/FileFromJavaDeserializationServlet.java
@@ -21,7 +21,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
